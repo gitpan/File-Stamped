85853de14467883e81b4a9e57492b434398978e8
KAN Fushihara <kan.fushihara@gmail.com>
2014-07-29 06:02:00 +0000

    Import of MIKIHOSHI/File-Stamped-0.08 from CPAN.
    gitpan-cpan-distribution: File-Stamped
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         MIKIHOSHI/File-Stamped-0.08.tar.gz
    gitpan-cpan-author:       MIKIHOSHI
    gitpan-cpan-maturity:     released

--- lib/File/Stamped.pm
+++ lib/File/Stamped.pm
@@ -2,7 +2,7 @@ package File::Stamped;
 use strict;
 use warnings;
 use 5.008001;
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 use Carp ();
 use POSIX ();
 use SelectSaver ();
@@ -73,7 +73,7 @@ sub _gen_symlink {
     my ($self, $fname) = @_;
 
     if (defined(my $symlink = *$self->{symlink})) {
-        if (-e $symlink) {
+        if (-l $symlink) {
             my $link = readlink $symlink;
             if (defined $link && $link ne $fname) {
                 unlink $symlink;
